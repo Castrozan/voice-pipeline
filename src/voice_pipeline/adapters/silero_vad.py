db7ce49b4944f70156d9b20f0d00e8f22fd28e85
Lucas de Castro Zanoni <lucas.zanoni@betha.com.br>
2026-02-14 00:24:27 -0300

    fix: support new Silero VAD model with state tensor input

--- src/voice_pipeline/adapters/silero_vad.py
+++ src/voice_pipeline/adapters/silero_vad.py
@@ -25,27 +25,49 @@ class SileroVad:
             providers=["CPUExecutionProvider"],
         )
         self._sample_rate = sample_rate
-        self._h = np.zeros((2, 1, 64), dtype=np.float32)
-        self._c = np.zeros((2, 1, 64), dtype=np.float32)
+
+        input_names = [i.name for i in self._session.get_inputs()]
+        self._use_state_tensor = "state" in input_names
+
+        if self._use_state_tensor:
+            state_shape = [i.shape for i in self._session.get_inputs() if i.name == "state"][0]
+            state_dim = state_shape[2] if len(state_shape) > 2 else 128
+            self._state = np.zeros((2, 1, state_dim), dtype=np.float32)
+        else:
+            hidden_dim = 64
+            self._h = np.zeros((2, 1, hidden_dim), dtype=np.float32)
+            self._c = np.zeros((2, 1, hidden_dim), dtype=np.float32)
+
         logger.info("Silero VAD loaded from %s", resolved_path)
 
     def process_frame(self, audio_frame: bytes) -> float:
         audio = np.frombuffer(audio_frame, dtype=np.int16).astype(np.float32) / 32767.0
         audio = audio[np.newaxis, :]
 
-        ort_inputs = {
-            "input": audio,
-            "h": self._h,
-            "c": self._c,
-            "sr": np.array([self._sample_rate], dtype=np.int64),
-        }
+        if self._use_state_tensor:
+            ort_inputs = {
+                "input": audio,
+                "state": self._state,
+                "sr": np.array(self._sample_rate, dtype=np.int64),
+            }
+            output, self._state = self._session.run(None, ort_inputs)
+        else:
+            ort_inputs = {
+                "input": audio,
+                "h": self._h,
+                "c": self._c,
+                "sr": np.array([self._sample_rate], dtype=np.int64),
+            }
+            output, self._h, self._c = self._session.run(None, ort_inputs)
 
-        output, self._h, self._c = self._session.run(None, ort_inputs)
         return float(output[0][0])
 
     def reset(self) -> None:
-        self._h = np.zeros((2, 1, 64), dtype=np.float32)
-        self._c = np.zeros((2, 1, 64), dtype=np.float32)
+        if self._use_state_tensor:
+            self._state = np.zeros_like(self._state)
+        else:
+            self._h = np.zeros_like(self._h)
+            self._c = np.zeros_like(self._c)
 
     @staticmethod
     def _download_model(target: Path) -> None:
